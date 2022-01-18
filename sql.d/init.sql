51ac9ed47765ed49c250a0bcd0287d704f2604f5
fridavbg <fperssontech@gmail.com>
2022-01-18 23:43:17 +0100

    db setup

---
+++ sql.d/init.sql
@@ -0,0 +1,8 @@
+CREATE USER 'doe'@'%' IDENTIFIED BY 'pass';
+GRANT ALL PRIVILEGES ON doe.* TO 'doe'@'%';
+
+CREATE DATABASE doe;
+USE doe;
+
+CREATE TABLE doe ( namn CHAR(10) );
+INSERT INTO doe VALUES ("Jane Doe"), ("John Doe");
