7c30d5a647a171eaf094fb9231f55261c24bd51c
fridavbg <fperssontech@gmail.com>
2022-01-31 12:34:26 +0100

    search.js

--- kmom02/terminal1/search_await.js
+++ kmom02/terminal1/search_await.js
@@ -1,17 +1,28 @@
 /**
- * A simple program to display information from the skolan database.
- *
- * @author frpe21
+ * Show teachers and their departments.
  */
 
-/**
- * Show information regarding teachers.
- * akronym, fornamn, efternamn, lon, kompetens, fodd
- */
 "use strict";
 
 const mysql = require("promise-mysql");
 const config = require("./config.json");
+
+// Read from commandline
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+// Promisify rl.question to question
+const util = require("util");
+
+rl.question[util.promisify.custom] = (arg) => {
+    return new Promise((resolve) => {
+        rl.question(arg, resolve);
+    });
+};
+const question = util.promisify(rl.question);
 
 /**
  * Main function.
@@ -19,42 +30,57 @@ const config = require("./config.json");
  * @async
  * @returns void
  */
+
 (async function () {
     const db = await mysql.createConnection(config);
     let str;
+    let search;
 
-    str = await viewTeachers(db);
+    search = await question("What to search for? ");
+    str = await searchTeachers(db, search);
     console.info(str);
 
+    rl.close();
     db.end();
 })();
 
 /**
- * Get a report with teacher details, formatted as a text table.
+ * Output resultset as formatted table with details on a teacher.
  *
  * @async
- * @param {connection} db Database connection.
+ * @param {connection} db     Database connection.
+ * @param {string}     search String to search for.
  *
  * @returns {string} Formatted table to print out.
  */
-async function viewTeachers(db) {
+async function searchTeachers(db, search) {
     let sql;
     let res;
     let str;
+    let like = `%${search}%`;
+
+    console.info(`Searching for: ${search}`);
 
     sql = `
-         SELECT
-             akronym,
-             fornamn,
-             efternamn,
-             avdelning,
-             lon,
-             kompetens,
-             fodd
-         FROM larare
-         ORDER BY akronym;
-     `;
-    res = await db.query(sql);
+        SELECT
+            akronym,
+            fornamn,
+            efternamn,
+            avdelning,
+            lon,
+            kompetens,
+            fodd
+        FROM larare
+        WHERE
+            akronym LIKE ?
+            OR fornamn LIKE ?
+            OR efternamn LIKE ?
+            OR avdelning LIKE ?
+            OR lon = ?
+            OR kompetens = ?
+        ORDER BY akronym;
+    `;
+    res = await db.query(sql, [like, like, like, like, like, search]);
     str = teacherAsTable(res);
     return str;
 }
