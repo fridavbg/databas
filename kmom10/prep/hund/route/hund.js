3e5212f7fd7f93322272e1fb85c5b0929de780d4
fridavbg <fperssontech@gmail.com>
2022-05-18 16:02:17 +0200

    omtenta hund webbklient init

---
+++ kmom10/prep/hund/route/hund.js
@@ -0,0 +1,43 @@
+/**
+ * Route for exam.
+ */
+"use strict";
+
+const express = require("express");
+const router = express.Router();
+const hund = require("../src/hund.js");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+const sitename = " | Hund";
+
+/**
+ * INDEX ROUTE
+ * /exam/index:
+ *   get:
+ *     summary: Display welcome page
+ *     description: Render welcome page
+ */
+router.get("/exam/index", (req, res) => {
+    let data = {
+        title: `Välkommen  ${sitename}`,
+    };
+
+    res.render("exam/", data);
+});
+
+/**
+ * About ROUTE
+ * /exam/about:
+ *   get:
+ *     summary: Display about page
+ *     description: Render information of creators
+ */
+router.get("/exam/about", async (req, res) => {
+    let data = {
+        title: `Om  ${sitename}`,
+    };
+
+    res.render("exam/about", data);
+});
+
+module.exports = router;
