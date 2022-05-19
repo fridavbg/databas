1bcb6d63e8bf5bf4cefa589c8c3fdd5b725191db
fridavbg <fperssontech@gmail.com>
2022-05-19 16:24:03 +0200

    omtenta hund klar

--- kmom10/prep/hund/sql/exam/backup.sql
+++ kmom10/prep/hund/sql/exam/backup.sql
@@ -356,6 +356,37 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `search_memberinfo` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb3 */ ;
+/*!50003 SET character_set_results = utf8mb3 */ ;
+/*!50003 SET collation_connection  = utf8mb3_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`dbadm`@`localhost` PROCEDURE `search_memberinfo`(
+    a_search VARCHAR(20)
+)
+BEGIN
+    SELECT *
+    FROM memberInfo AS mI
+WHERE 
+    mI.fornamn LIKE CONCAT('%', a_search, '%') OR
+    mI.efternamn LIKE CONCAT('%', a_search, '%') OR
+    mI.alias LIKE CONCAT('%', a_search, '%') OR
+    mI.ort LIKE CONCAT('%', a_search, '%') OR
+    mI.namn LIKE CONCAT('%', a_search, '%') OR
+    mI.ras LIKE CONCAT('%', a_search, '%') OR
+    mI.godkänd LIKE CONCAT('%', a_search, '%')
+    ;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `show_concertinfo` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -459,4 +490,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2022-05-19 15:48:19
+-- Dump completed on 2022-05-19 16:23:11
